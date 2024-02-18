3e005edff780dc4872708196765483148303f769
nnda <96333146+nndda@users.noreply.github.com>
2024-02-19 03:15:32 +0800

    Update vendor.ts

---
+++ src/vendor.ts
@@ -0,0 +1,9 @@
+// normalize.css
+import "normalize.css";
+
+// Fonts
+import "@fontsource/noto-sans-jp";
+import "@fontsource/fira-code";
+
+// Icons
+import "@fortawesome/fontawesome-free/css/all.min.css";
