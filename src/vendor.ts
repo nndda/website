f65fcec4b8bf490fc307ae9dfc16a35f5d04e615
nnda <96333146+nndda@users.noreply.github.com>
2024-02-21 01:39:35 +0800

    Add placeholder font

--- src/vendor.ts
+++ src/vendor.ts
@@ -4,6 +4,7 @@ import "normalize.css";
 // Fonts
 import "@fontsource/noto-sans-jp";
 import "@fontsource/fira-code";
+import "@fontsource/flow-rounded";
 
 // Icons
 import "@fortawesome/fontawesome-free/css/all.min.css";
