aef95b1ccb2ae5435ad1d854f9c4a189367842d5
nnda <96333146+nndda@users.noreply.github.com>
2024-02-28 15:45:43 +0800

    Execute scripts at `onload`

--- src/scripts/parallax.ts
+++ src/scripts/parallax.ts
@@ -1,11 +1,15 @@
-const Parallax = require("parallax-js"),
-    scene = document.getElementById("intro-cover-images");
+window.addEventListener("load", () => {
+    const Parallax = require("parallax-js"),
+        scene = document.getElementById("intro-cover-images");
 
-let parallaxInstance = new Parallax(scene, {
-    scalarY: 8.0,
-});
+    let parallaxInstance = new Parallax(scene, {
+        scalarY: 2.0,
+        scalarX: 2.0,
+    });
 
-// const simpleParallax = require("simple-parallax-js");
-// const scrollParallaxElems = document.querySelectorAll("#intro > .scroll-parallax");
-// const scrollParallaxElems = document.querySelectorAll(".project");
-// new simpleParallax(scrollParallaxElems);
+    // const simpleParallax = require("simple-parallax-js");
+    // const scrollParallaxElems = document.querySelectorAll("#intro > .scroll-parallax");
+    // const scrollParallaxElems = document.querySelectorAll(".project");
+    // new simpleParallax(scrollParallaxElems);
+
+})
--- src/scripts/scroll.ts
+++ src/scripts/scroll.ts
@@ -1,48 +1,52 @@
-const controlBar = document.querySelector(".control-bar")
-const controlBarTitle = document.getElementById("control-bar-title");
+window.addEventListener("load", () => {
+  const controlBar = document.querySelector(".control-bar")
+  const controlBarTitle = document.getElementById("control-bar-title");
 
-const homeOverlayHeading = document.querySelector(".cover-overlay-heading");
+  const homeOverlayHeading = document.querySelector(".cover-overlay-heading");
+  const introCover = document.querySelector(".intro-cover-container");
 
-const codeTagStart = document.querySelector(".code-tag-start");
-const codeTagEnd = document.querySelector(".code-tag-end");
+  const codeTagStart = document.querySelector(".code-tag-start");
+  const codeTagEnd = document.querySelector(".code-tag-end");
 
-const sectProjects = document.getElementById("projects");
-const sectIllust = document.getElementById("illustrations");
+  const sectProjects = document.getElementById("projects");
+  const sectIllust = document.getElementById("illustrations");
 
-const observer = new IntersectionObserver( 
-  ([e]) => {
-    let intersected = e.intersectionRatio < 1;
-    e.target.classList.toggle("sticky", intersected);
-    ctaScrollDown.classList.toggle("hidden", intersected);
-    homeOverlayHeading.classList.toggle("collapsed", intersected);
-    codeTagStart.classList.toggle("rotate-reset", intersected);
-    codeTagEnd.classList.toggle("rotate-reset", intersected);
-  },
-  {
-    rootMargin: "-1px 0px 10px 0px",
-    threshold: [1],
+  const observer = new IntersectionObserver( 
+    ([e]) => {
+      let intersected = e.intersectionRatio < 1;
+      e.target.classList.toggle("sticky", intersected);
+      ctaScrollDown.classList.toggle("hidden", intersected);
+      homeOverlayHeading.classList.toggle("collapsed", intersected);
+      introCover.classList.toggle("dim", intersected);
+      codeTagStart.classList.toggle("rotate-reset", intersected);
+      codeTagEnd.classList.toggle("rotate-reset", intersected);
+    },
+    {
+      rootMargin: "-1px 0px 10px 0px",
+      threshold: [1],
+    }
+  );
+
+  observer.observe(controlBar);
+
+  function controlBarScrollContents() {
+    if (sectIllust.getBoundingClientRect().y < 0) {
+      controlBarTitle.textContent = "illustrations";
+      controlBar.setAttribute("style", "--tint-color: #fc3a78");
+
+    } else if (sectProjects.getBoundingClientRect().y < 0) {
+      controlBarTitle.textContent = "projects";
+      controlBar.setAttribute("style", "--tint-color: #3ae5f9");
+
+    } else {
+      controlBarTitle.textContent = "intro";
+      controlBar.setAttribute("style", "--tint-color: #eaeaea");
+    }
   }
-);
 
-observer.observe(controlBar);
+  window.addEventListener("scroll", () => controlBarScrollContents());
+  document.addEventListener("load", () => controlBarScrollContents());
 
-function controlBarScrollContents() {
-  if (sectIllust.getBoundingClientRect().y < 0) {
-    controlBarTitle.textContent = "illustrations";
-    controlBar.setAttribute("style", "--tint-color: #fc3a78");
-
-  } else if (sectProjects.getBoundingClientRect().y < 0) {
-    controlBarTitle.textContent = "projects";
-    controlBar.setAttribute("style", "--tint-color: #3ae5f9");
-
-  } else {
-    controlBarTitle.textContent = "intro";
-    controlBar.setAttribute("style", "--tint-color: #eaeaea");
-  }
-}
-
-controlBarScrollContents()
-window.addEventListener("scroll", () => controlBarScrollContents())
-
-const ctaScrollDown = document.getElementById("cta-scroll-down");
-ctaScrollDown.addEventListener("click", () => window.scrollBy(0, 300))
+  const ctaScrollDown = document.getElementById("cta-scroll-down");
+  ctaScrollDown.addEventListener("click", () => window.scrollBy(0, 300));
+})
