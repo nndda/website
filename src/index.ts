d2992782fa276448374705a38ef140d0c651acb7
nnda <96333146+nndda@users.noreply.github.com>
2024-02-28 15:58:16 +0800

    Update main styles

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,7 @@
-// Styles
-// import "./styles/main.scss";
-// import "./styles/home.scss";
-// import "./styles/projects.scss";
-// import "./styles/illustrations.scss";
-// import "./styles/tools.scss";
-
 // Scripts
 import "./scripts/parallax.ts";
 import "./scripts/scroll.ts";
 import "./scripts/chart.ts";
 
+// Styles
+import "./styles/main.scss"
